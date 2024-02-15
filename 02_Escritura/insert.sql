c237424d758e6d252a6e8fe005c3872cd31bd59e
Mablenn <marcos.broseta@gmail.com>
2024-02-15 14:25:36 +0100

    Update insert.sql

--- 02_Escritura/insert.sql
+++ 02_Escritura/insert.sql
@@ -3,5 +3,5 @@
   La sentencia INSERT nos permite insertar valores en las tablas de la base de datos.
 */
 
--- Inseta un registro a la tabla productos. Este registro está compuesto por id, descripcion, proveedor
+-- Inserta un registro en la tabla productos. Este registro está compuesto por id, descripcion, proveedor
 INSERT INTO productos (id, descripcion, proveedor) VALUES (0115, PD334, PROV25)
