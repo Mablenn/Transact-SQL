2ce7236aec5ea6299c3bd6d36478b74ffd4d7fbf
Mablenn <marcos.broseta@gmail.com>
2024-03-06 17:58:03 +0100

    Update update.sql

--- 01_Data_Manipulation_Language/update.sql
+++ 01_Data_Manipulation_Language/update.sql
@@ -1,4 +1,10 @@
-/^
+/*
   UPDATE  
   Actualización de registros.
 */
+
+
+-- Actualiza el campo importe de la tabla compras aumentando en 25 los registros donde la fecha de compra sea mayor de 2024-03-01
+UPDATE proveedores
+SET importe = importe + 25
+WHERE fecha > 2024-03-01
