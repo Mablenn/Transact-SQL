15b5537b90fc38c3a74e8a2719071807e7765782
Mablenn <marcos.broseta@gmail.com>
2024-03-08 13:16:26 +0100

    Update alter.sql

--- 02_Data_Definition_Language/alter.sql
+++ 02_Data_Definition_Language/alter.sql
@@ -3,5 +3,8 @@
     La sentencia ALTER permite modificar las propiedades de una tabla
 */
 
---Añade una nueva columna categoria de tipo entero a la tabla Productos
-  ALTER TABLE Productos ADD categoria INT
+    --Añade una nueva columna categoria de tipo entero a la tabla Productos
+      ALTER TABLE Productos ADD categoria INT
+
+      --Modifica la columna fecha al tipo de dato smalldatetime
+    ALTER TABLE dbo.compras ALTER COLUMN fecha smalldatetime
