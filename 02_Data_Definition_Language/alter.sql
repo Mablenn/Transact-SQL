6c3dacc02f165c78268e54124bc8b09156c40c2d
Mablenn <marcos.broseta@gmail.com>
2024-03-08 13:06:14 +0100

    Update alter.sql

--- 02_Data_Definition_Language/alter.sql
+++ 02_Data_Definition_Language/alter.sql
@@ -1,1 +1,7 @@
+/*
+    ALTER
+    La sentencia ALTER permite modificar las propiedades de una tabla
+*/
 
+--Añade una nueva columna categoria de tipo entero a la tabla Productos
+  ALTER TABLE Productos ADD categoria INT
