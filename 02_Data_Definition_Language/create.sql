b8db1323719c57d9832b776582a3b5dbad59288f
Mablenn <marcos.broseta@gmail.com>
2024-03-08 13:00:36 +0100

    Update create.sql

--- 02_Data_Definition_Language/create.sql
+++ 02_Data_Definition_Language/create.sql
@@ -1,1 +1,25 @@
+/*
+  CREATE
+  Al crear una tabla debemos asignar un nombre a ésta, un nombre por cada columna y el tipo de dato
+  que contendrán los regsitros de cada columna.
 
+  CREATE TABLE nombre_tabla
+  (
+      nombre_columna1  tipo_dato,
+      nombre_columna2  tipo_dato,
+      nombre_columna3  tipo_dato,
+  )
+
+*/
+
+CREATE TABLE Productos
+(
+    --Primera columna
+    productoId INT IDENTITY(1,1),
+    --Segunda columna
+    proveedorId INT,
+    --Tercera columna
+    precioProducto FLOAT(2,8),
+    --Cuarta columna
+    descripcion VARCHAR(25)
+ )
