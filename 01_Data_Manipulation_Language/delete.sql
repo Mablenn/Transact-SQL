b0f16d572a16a720ecea0f2b9bcfcf1836efb124
Mablenn <marcos.broseta@gmail.com>
2024-03-10 18:26:57 +0100

    Update delete.sql

--- 01_Data_Manipulation_Language/delete.sql
+++ 01_Data_Manipulation_Language/delete.sql
@@ -1,4 +1,8 @@
 /*
   DELETE
-
+  Elimina registros de tablas de una base de datos
 */
+
+-- Elimina de la tabla los importes cuyos valores se encuentren entre 20 y 80
+DELETE compras
+WHERE importe BETWEEN 20 AND 80
