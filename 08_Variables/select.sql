45286a28849892fbb5e9fb71193b87fdf946099d
Mablenn <marcos.broseta@gmail.com>
2024-07-22 09:48:11 +0200

    Update select.sql

--- 08_Variables/select.sql
+++ 08_Variables/select.sql
@@ -1,1 +1,9 @@
+/* 
+  SELECT para asignar valores a variables.
 
+  Además de utilizar SELECT para imprimir valores, también permite utilizarla para asignar valores a variables.
+  El modo es similar a la sentencia SET, la diferencia entre ambas es que la sentencia SET sólo puede asignar un
+  valor a una sola variable, en cambio, SELECT premite una asignación múltiple de valores a variables en una
+  sola instrucción.
+
+*/
