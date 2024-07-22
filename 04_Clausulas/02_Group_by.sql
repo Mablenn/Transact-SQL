33f3c9a58f8812926b6b1a8be6216d66ff075612
Mablenn <marcos.broseta@gmail.com>
2024-07-22 08:59:44 +0200

    Update 02_Group_by.sql

--- 04_Clausulas/02_Group_by.sql
+++ 04_Clausulas/02_Group_by.sql
@@ -4,7 +4,7 @@
     y agrupa el resultado de una consulta en grupos de filas que comparten un mismo valor. En este caso
     la consulta SELECT devuelve una sola fila por cada grupo de filas del mismo valor.
     Si utilizamos la cláusula GROUP BY, todas las columnas que se especifiquen en la sentencia SELECT también
-    deben estar GROUP BY. En cambio, no ocurre lo mismo al revés. Las columnas que se especifiquen en
+    deben estar en GROUP BY. En cambio, no ocurre lo mismo al revés. Las columnas que se especifiquen en
     GROUP BY no necesariamente deben estar en el SELECT.
     Normalmente, esta cláusula se utiliza con funciones de agregación como SUM, AVG, COUNT, MAX y MIN.
 */
