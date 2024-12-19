a9aa4b89f93cbf131a542f086a2eeedc5e46cfee
Mablenn <marcos.broseta@gmail.com>
2024-12-19 13:25:20 +0100

    Update delete.sql

--- 01_Data_Manipulation_Language/delete.sql
+++ 01_Data_Manipulation_Language/delete.sql
@@ -3,6 +3,6 @@
   Elimina registros de tablas de una base de datos
 */
 
--- Elimina de la tabla los importes cuyos valores se encuentren entre 20 y 80
-DELETE compras
+-- Elimina de la tabla dbo.compras los importes cuyos valores se encuentren entre 20 y 80
+DELETE dbo.compras
 WHERE importe BETWEEN 20 AND 80
