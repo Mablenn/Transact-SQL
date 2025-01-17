424b6fbc20fa933667fed15cab05547a9d14cdab
Mablenn <marcos.broseta@gmail.com>
2025-01-17 09:53:56 +0100

    Update select.sql

--- 01_Data_Manipulation_Language/select.sql
+++ 01_Data_Manipulation_Language/select.sql
@@ -1,6 +1,6 @@
 /*
   SELECT
-  La sentencia SELECT obtiene información de las tablas de una base de datos.
+  El uso más común de la sentencia SELECT es para obtener información de las tablas de una base de datos.
 */
 
 -- Obtiene todos los registros de la tabla productos
@@ -14,3 +14,8 @@ FROM productos;
 SELECT id, descripcion
 FROM productos
 WHERE id = 122
+
+-- Además de utilizar SELECT para recuperar datos, también se puede usar para realizar operaciones matemáticas
+SELECT 3 * 5 AS MULTIPLICA, 10 / 2 AS DIVIDE
+-- o mostrar texto.
+SELECT 'Mensaje de texto' AS Mensaje
