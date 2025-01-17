e8eea2f6fb450586424cdef9fbe0c93d26c21e2a
Mablenn <marcos.broseta@gmail.com>
2025-01-17 10:12:17 +0100

    Update Truncate.sql

--- 01_Data_Manipulation_Language/Truncate.sql
+++ 01_Data_Manipulation_Language/Truncate.sql
@@ -1,6 +1,6 @@
 /*
   TRUNCATE
-  La sentencia TRUNCATE permite eliminar registros de una tabla. A diferencia de la sentencia 
+  La sentencia TRUNCATE permite eliminar registros de una tabla y reiniciar los contadores de autoincremento, si los tiene. A diferencia de la sentencia 
   DELETE, tiene algunas restricciones:
     - No se puede utilizar con una clásula WHERE.
     - La tabla no puede tener claves foráneas.
@@ -8,4 +8,6 @@
 
   -- Elimina todos los registros de la tabla Proveedores
   TRUNCATE TABLE Proveedores
-  
+
+  -- Elimina todos los registros de las tablas Productos y Ventas
+  TRUNCATE TABLE Productos, Ventas
