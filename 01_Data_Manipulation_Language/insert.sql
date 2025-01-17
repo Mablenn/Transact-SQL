c556eae9a0429b6de74782b5240c5282993a0bd8
Mablenn <marcos.broseta@gmail.com>
2025-01-17 10:52:09 +0100

    Update insert.sql

--- 01_Data_Manipulation_Language/insert.sql
+++ 01_Data_Manipulation_Language/insert.sql
@@ -5,4 +5,13 @@
 
 -- Inserta un registro en la tabla productos. Este registro está compuesto por id, descripcion, proveedor
 INSERT INTO productos (id, descripcion, proveedor) 
-VALUES (0115, PD334, PROV25)
+VALUES (0115, 'PD334', 'PROV25')
+
+-- Insertar varios registros de una sola vez
+INSERT INTO productos (id, descripcion, proveedor)
+VALUES
+  (0122, 'HH002', 'PROV25'),
+  (0200, 'HH002', 'PROV22'),
+  (0223, 'PD322', 'PROV11'),
+  (1134, 'PD322', 'PROV25')
+  
