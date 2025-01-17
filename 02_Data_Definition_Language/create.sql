3410e8b7246267aa160aac465c6d0a30ffacfbc3
Mablenn <marcos.broseta@gmail.com>
2025-01-17 11:59:27 +0100

    Update create.sql

--- 02_Data_Definition_Language/create.sql
+++ 02_Data_Definition_Language/create.sql
@@ -15,11 +15,23 @@
 CREATE TABLE Productos
 (
     --Primera columna
-    productoId INT PRIMARY KEY IDENTITY(1,1) NOT NULL,
+    productoId INT PRIMARY KEY,
     --Segunda columna
-    proveedorId SMALLINT NOT NULL,
+    proveedorId SMALLINT,
     --Tercera columna
     precioProducto FLOAT(2,8),
     --Cuarta columna
     descripcion VARCHAR(25)
- )
+ );
+
+-- Cuando se crea una tabla, se puede utilizar restricciones para garantizar la integridad de los datos.
+CREATE TABLE Productos
+(
+    productoId INT PRIMARY KEY IDENTITY(1,1), -- Clave primaria, autoincremento
+    proveedorId SMALLINT NOT NULL, -- No admite valor nulo
+    precioProducto FLOAT(2,8) CHECK (precioProducto > 0), --  Todos los precios deben ser mayor que 0
+    descripcion VARCHAR(25)
+    FOREIGN KEY (proveedorId) REFERENCES Proveedores(proveedorId) -- Establece relación con la tabla Proveedores
+ );
+
+
