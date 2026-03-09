1e90daf0201b7221b4bf08149e8436643997071d
Mablen <marcos.broseta@gmail.com>
2026-03-09 17:23:51 +0100

    Create 03_try_cast.sql
    Added comments explaining TRY_CAST function and its usage.

--- 06_Funciones/03_try_cast.sql
+++ 06_Funciones/03_try_cast.sql
@@ -1,1 +1,10 @@
 
+/*
+    TRY_CAST
+    Esta función es parecida a CAST pero más potente. En caso que la conversión de datos no sea posible
+    no lanzará un error, sino que captura el error internamente y convierte el tipo de dato en un valor NULL.
+*/
+
+-- Selecciona solo los registros donde 'Codigo' sea realmente un número
+SELECT * FROM Ventas
+WHERE TRY_CAST(Codigo AS INT) IS NOT NULL;
