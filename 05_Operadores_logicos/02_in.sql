fe6a1a88482b1f7aadba052cf0cf89465bf35ec9
Mablen <marcos.broseta@gmail.com>
2026-03-09 18:34:28 +0100

    Enhance SQL script with operator explanations
    Added comments explaining the IN() and NOT IN() operators in SQL.

--- 05_Operadores_logicos/02_in.sql
+++ 05_Operadores_logicos/02_in.sql
@@ -1,3 +1,23 @@
 /*
     IN()
+    Este operador devuelve registros donde el valor de una columna coincida con los valores
+    que se pasan en una lista. En este caso, la lista, son los valores que están dentro de los
+    paréntesis.
+    Este operador lo podemos considerar como una múltiples condiciones OR, de este modo ahorramos código y tiempo.
+    Sintaxis:
+        -Si utilizamos números: IN(33, 434, 456)
+        -Si utilizamos caracteres esto van entre comillas simples '': IN('Grecia', 'Francia', 'Suiza')
 */
+
+-- Obtén el nombre  de proveedores que estén en Alemania, Francia o Italia
+SELECT nombre
+FROM Proveedores
+WHERE pais IN('Alemania', 'Francia', 'Italia')
+
+-- También se puede utilizar para excluir registros utilizando NOT IN()
+--  Selecciona los empleados que no pertenezcan a los departamentos IT o Finanzas
+SELECT *
+FROM Empleados
+WHERE departamento NOT IN ('IT', 'Finanzas')
+
+
